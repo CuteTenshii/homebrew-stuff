bf1f8689393c9d277558ad95502895809d93cd8a
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2025-05-25 19:02:24 +0200

    initial commit

---
+++ Casks/deezer-discord-rpc.rb
@@ -0,0 +1,21 @@
+cask "deezer-discord-rpc" do
+  name "Deezer Discord RPC"
+  desc "A Discord RPC for Deezer"
+  homepage "https://github.com/JustYuuto/deezer-discord-rpc"
+  version :latest
+  sha256 :no_check
+
+  on_intel do
+    url "https://github.com/JustYuuto/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-x64.dmg"
+  end
+  on_arm do
+    url "https://github.com/JustYuuto/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-arm64.dmg"
+  end
+
+  app "Deezer Discord RPC.app"
+
+  zap trash: [
+    "~/Library/Application Support/deezer-discord-rpc",
+    "~/Library/Preferences/com.github.yuuto.deezerdiscordrpc.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Stuff
+
+This repository contains a collection of Homebrew formulae and casks that I maintain.
+
+## Usage
+
+```shell
+brew tap justyuuto/stuff
+```
+
+## Casks
+
+### Deezer Discord RPC
+
+This cask is for the [Deezer Discord RPC](https://github.com/JustYuuto/deezer-discord-rpc).
+
+```shell
+brew install --cask deezer-discord-rpc
+```
