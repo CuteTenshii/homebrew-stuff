0ec6f7bddac4c4ee74f5ccc635e68c0187458277
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2025-07-10 19:54:24 +0200

    fix: update username

--- Casks/deezer-discord-rpc.rb
+++ Casks/deezer-discord-rpc.rb
@@ -1,15 +1,15 @@
 cask "deezer-discord-rpc" do
   name "Deezer Discord RPC"
   desc "A Discord RPC for Deezer"
-  homepage "https://github.com/JustYuuto/deezer-discord-rpc"
+  homepage "https://github.com/CuteTenshii/deezer-discord-rpc"
   version :latest
   sha256 :no_check
 
   on_intel do
-    url "https://github.com/JustYuuto/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-x64.dmg"
+    url "https://github.com/CuteTenshii/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-x64.dmg"
   end
   on_arm do
-    url "https://github.com/JustYuuto/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-arm64.dmg"
+    url "https://github.com/CuteTenshii/deezer-discord-rpc/releases/latest/download/DeezerDiscordRPC-mac-arm64.dmg"
   end
 
   app "Deezer Discord RPC.app"
